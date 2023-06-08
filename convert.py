d569be4a819759b660c407189fcfc0a2c7e4ed9f
Kui <kui.zenith@gmail.com>
2023-06-08 23:07:20 +0800

    updated convert.py

--- convert.py
+++ convert.py
@@ -3,11 +3,11 @@ import soundfile as sf
 import numpy as np
 
 def convert(input_file, output_file):
-  data, samplerate = sf.read(input_file)
-  sf.write(output_file, data[:, 0], samplerate, subtype="FLOAT")
+  data, sample_rate = sf.read(input_file)
+  sf.write(output_file, data[:, 0].astype(np.float32), 24000, format="WAV", subtype="FLOAT")
   print("Conversion completed successfully.")
 
-directory = "./data/bark - human/human-1"
+directory = "./data/bark - human/human-2"
 
 for fn in os.listdir(directory):
   convert(f"{directory}/{fn}", f"{directory}/{fn}")
